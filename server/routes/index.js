59220f0d7d43025e29928502e8077dd1f46ea2c8
heavenlist <heavenlist@heavenlistdeMacBook-Pro.local>
2019-12-07 21:56:27 -0500

    server try

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,15 +3,37 @@ const db = require('../db');
 const router  = express.Router();
 
 router.get('/', async (req, res, next)=>{
-    //res.json({test:  'test'});
+    // try{
+    //     let results = await db.all();
+    //     res.json(results);
+    // }catch(e){
+    //     console.log(e);
+    //     res.sendStatus(500);
+    // }
 
-    try{
-        let results = await db.all();
-        res.json(results);
-    }catch(e){
-        console.log(e);
-        res.sendStatus(500);
-    }
-})
+    // const {name, pword} = req.query;
+    // const user_info = {
+    //    name, pword
+    // };
+    // console.log(name, pword);
+    // res.send(user_info);
+    const {email, password} = req.query;
+    console.log(email, password);
+    res.send(`log in`);
+
+    //res.json({test:  'test'});
+});
+
+// router.get('/api/login', async (req, res, next)=>{
+//     res.json({test:  'test'});
+
+//    const {name, pword} = req.query;
+//    const user_info = {
+//        name, pword
+//    };
+//    console.log(name, pword);
+//    res.send(`log in`);
+//});
+
 
 module.exports = router;
