9b0ca34ebe2e3145fd7a382d966ba9d2aab06682
heavenlist <heavenlist@heavenlistdeMBP.lan>
2019-12-09 22:53:50 -0500

    startmessgewith

--- server/db/index.js
+++ server/db/index.js
@@ -10,6 +10,22 @@ const conn = mysql.createPool({
 
 let db = {};
 
+db.StartMessageWith = (user, target, type, title, body) =>{
+  return new Promise((resolve, reject) => {
+    conn.query(
+      `call StartMessageWith(?, ?, ?, ?, ?)`,
+      [user, target, type, title, body],
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(results);
+      }
+    );
+  });
+};
+
 db.StartMessageIn = (user, type, title, body) =>{
   return new Promise((resolve, reject) => {
     conn.query(
