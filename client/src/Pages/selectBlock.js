311137c0b64f5acfabccf399c33c3c1f7b0a866d
Hanyu Xu <xhyalex@gmail.com>
2019-12-19 07:09:27 -0500

    news finished

--- client/src/Pages/selectBlock.js
+++ client/src/Pages/selectBlock.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class selectBlock extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      user: this.props.location.state.user
+    };
   }
 
   render() {
