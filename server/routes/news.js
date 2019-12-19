cded08d4ff00965f4dbab13ffe70ca0509c91a81
heavenlist <heavenlist@heavenlistdeMacBook-Pro.local>
2019-12-18 20:05:59 -0500

    News: friend + blocl

--- server/routes/news.js
+++ server/routes/news.js
@@ -13,11 +13,23 @@ router.get("/", async (req, res, next) => {
   const { email } = req.query;
   try {
 
-    let news_info = await db.listAllNews(email);
-    var news_string = JSON.stringify(news_info);
-    var news_json = JSON.parse(news_string)[0];
-    console.log(news_json);
-    res.send({news: news_json});
+    // let news_info = await db.listAllNews(email);
+    // var news_string = JSON.stringify(news_info);
+    // var news_json = JSON.parse(news_string)[0];
+
+
+    let friend_news = await db.listAllFriendRequests(email);
+    var friend_new_string = JSON.stringify(friend_news);
+    var friend_new_json = JSON.parse(friend_new_string)[0];
+
+    let block_news = await db.listAllBlockRequests(email);
+    var block_new_string = JSON.stringify(block_news);
+    var block_new_json = JSON.parse(block_new_string)[0];
+
+    console.log(friend_new_json);
+    console.log(block_new_json);
+    //console.log(news_json);
+    res.send({friend: friend_new_json,block:  block_new_json});
 
   } catch (e) {
     console.log(e);
